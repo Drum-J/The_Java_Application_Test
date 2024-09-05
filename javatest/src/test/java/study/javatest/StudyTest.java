aac7450989f7921871c1339feabe303c96d4acdd
DrumJ <tmdgh717@naver.com>
2024-09-05 15:41:43 +0900

    JUnit 5: 태깅과 필터링

--- javatest/src/test/java/study/javatest/StudyTest.java
+++ javatest/src/test/java/study/javatest/StudyTest.java
@@ -7,13 +7,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.DisabledOnJre;
-import org.junit.jupiter.api.condition.DisabledOnOs;
-import org.junit.jupiter.api.condition.EnabledOnJre;
-import org.junit.jupiter.api.condition.EnabledOnOs;
-import org.junit.jupiter.api.condition.JRE;
-import org.junit.jupiter.api.condition.OS;
 
 import java.time.Duration;
 
@@ -22,7 +17,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTimeout;
-import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -30,8 +24,7 @@ class StudyTest {
 
     @Test
     @DisplayName("스터디 만들기")
-    //@EnabledOnOs(OS.WINDOWS)
-    @EnabledOnJre({JRE.JAVA_8, JRE.JAVA_17, JRE.JAVA_21})
+    @Tag("fast")
     void create_new_study() throws Exception {
         //given
         Study study = new Study(10);
@@ -40,10 +33,6 @@ class StudyTest {
         //then
         assertAll(
                 () -> assertNotNull(study),
-                // 파라미터 순서 : 기댓값(expected), 실제값(actual) 하지만 순서가 바껴도 테스트는 제대로 동작함.
-                // 또한 메세지 자리에 Supplier 가 들어오기 때문에 람다로 사용가능 하다.
-                // 왜 Supplier, 람다를 사용할까? Message 를 String 으로 그대로 넣으면 성공,실패 관계없이 Message 연산을 항상 실행하지만
-                // 람다를 사용하게 되면 실패할 경우에만 Message 연산을 실행한다고 한다.
                 () ->assertEquals(StudyStatus.DRAFT,study.getStatus(),
                         () -> "스터디를 처음 만들면 " + StudyStatus.DRAFT + " 상태다."),
                 () -> assertTrue(study.getLimit() > 0 ,"스터디 최대 참석 가능 인원은 0보다 커야한다.")
@@ -52,8 +41,8 @@ class StudyTest {
     }
 
     @Test
-    //@DisabledOnOs(OS.WINDOWS)
-    @DisabledOnJre({JRE.JAVA_8, JRE.JAVA_17, JRE.JAVA_21})
+    @DisplayName("스터디 정원 TEST")
+    @Tag("fast")
     void create_new_study_exception() {
         IllegalArgumentException exception =
                 assertThrows(IllegalArgumentException.class, () -> new Study(-10));
@@ -61,15 +50,16 @@ class StudyTest {
     }
 
     @Test
+    @DisplayName("스터디 생성 시간 TEST")
+    @Tag("slow")
     void timeout() {
         assertTimeout(Duration.ofMillis(100), () -> {
             new Study(10);
             Thread.sleep(50);
         });
-        // 실패 시 즉시 끝내고 싶다 하면 assertTimeoutPreemptively 를 사용하면 100 밀리세컨드 이후에 바로 종료된다.
     }
 
-    @BeforeAll // static void 로 작성해야 한다. private 안되고, default public 가능, return 타입이 있어선 안된다.
+    @BeforeAll
     static void beforeAll() {
         System.out.println("before all");
     }
