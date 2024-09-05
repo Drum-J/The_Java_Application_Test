faf0298aa7eb70dc97e212c425e8712cc487ea0f
DrumJ <tmdgh717@naver.com>
2024-09-05 14:18:43 +0900

    JUnit 시작하기

--- javatest/src/test/java/study/javatest/StudyTest.java
+++ javatest/src/test/java/study/javatest/StudyTest.java
@@ -1,5 +1,10 @@
 package study.javatest;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,6 +19,32 @@ class StudyTest {
 
         //then
         assertNotNull(study);
+        System.out.println("create");
     }
 
+    @Test
+    @Disabled
+    void create1() {
+        System.out.println("create1");
+    }
+
+    @BeforeAll // static void 로 작성해야 한다. private 안되고, default public 가능, return 타입이 있어선 안된다.
+    static void beforeAll() {
+        System.out.println("before all");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("after all");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("before each");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("after each");
+    }
 }
