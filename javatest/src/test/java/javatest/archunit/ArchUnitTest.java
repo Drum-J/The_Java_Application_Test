ff84a60bd9d68f10556b35ec412449685d712fce
DrumJ <tmdgh717@naver.com>
2024-11-04 22:23:22 +0900

    추가로 알아본 Test Package 제외하는 방법

--- javatest/src/test/java/javatest/archunit/ArchUnitTest.java
+++ javatest/src/test/java/javatest/archunit/ArchUnitTest.java
@@ -2,11 +2,12 @@ package javatest.archunit;
 
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.lang.ArchRule;
-import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
 import org.junit.jupiter.api.Test;
 
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.*;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
 
 public class ArchUnitTest {
@@ -14,12 +15,14 @@ public class ArchUnitTest {
     @Test
     void packageDependencyTest() throws Exception {
         //given
-        JavaClasses classes = new ClassFileImporter().importPackages("javatest");
+        JavaClasses classes = new ClassFileImporter()
+                .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS) // 테스트 패키지 제외
+                .importPackages("javatest");
 
         //when
         ArchRule domainPackageRule = classes().that().resideInAPackage("..domain..")
                 .should().onlyBeAccessed().byClassesThat()
-                .resideInAnyPackage("..study..", "..member..", "..domain..", "..docker..", "..javatest..");
+                .resideInAnyPackage("..study..", "..member..", "..domain..");
 
         //then
         domainPackageRule.check(classes);
@@ -42,10 +45,12 @@ public class ArchUnitTest {
     @Test
     void studyPackageRule() throws Exception {
         //given
-        JavaClasses classes = new ClassFileImporter().importPackages("javatest");
+        JavaClasses classes = new ClassFileImporter()
+                .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS) // 테스트 패키지 제외
+                .importPackages("javatest");
 
         //when
-        ArchRule studyPackageRule = noClasses().that().resideOutsideOfPackages("..study..","..docker..")
+        ArchRule studyPackageRule = noClasses().that().resideOutsideOfPackages("..study..")
                 .should().accessClassesThat().resideInAPackage("..study..");
         //then
         studyPackageRule.check(classes);
